a73dba91ecd456f88e91443f93ff55eb16cc277b
Nate Levesque <nate@thenaterhood.com>
2021-06-16 08:12:01 -0400

    version bump

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ suggested = {
         }
 
 setup(name='python-autocite',
-    version='0.0.2',
+    version='0.0.3',
     description='Scrape webpages and generate citations',
     author='Nate Levesque',
     author_email='public@thenaterhood.com',
